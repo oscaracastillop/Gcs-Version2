c8f51bb0201bd518dbd89130bfea0f82cd8564bb
oscar.castillo <oscar.castillo@DESKTOP-98NLHSV>
2025-07-30 15:13:38 -0500

    xd

--- App/JsApp/NominaEmpleado.js
+++ App/JsApp/NominaEmpleado.js
@@ -11,14 +11,39 @@
         $("#TituloModalNominaEmpleado").empty().append('<label>CREAR NOMINA EMPLEADO</label>');
         $('#ModalNominaEmpleado').modal('show');
         $("#SelectEstadoNominaEmpleado").hide();
-        $("#BotonesModalNominaEmpleado").empty().append('<button type="button" class="btn btn-sm btn-modal-Cancelar" data-bs-dismiss="modal" aria-label="Close">Cancelar</button>' + '<button type="button" class="btn btn-sm btn-modal-guardar" onclick="CrearNominaEmpleado()">Generar Nómina</button>');
+        $("#BotonesModalNominaEmpleado").empty().append('<button class="btn btn-sm btn-modal-guardar" onclick="CrearNominaEmpleado()">Generar Nómina</button>');
     } if (tipo == 'E') {
         $("#TituloModalNominaEmpleado").empty().append('<label>EDITAR NOMINA EMPLEADO</label>');
         $('#ModalNominaEmpleado').modal('show');
         $("#SelectEstadoNominaEmpleado").show();
-        $("#BotonesModalNominaEmpleado").empty().append('<button type="button" class="btn btn-sm btn-modal-Cancelar" data-bs-dismiss="modal" aria-label="Close">Cancelar</button>' + '<button type="button" class="btn btn-sm btn-modal-guardar" onclick="ActualizarNominaEmpleado()">Guardar Cambios</button>');
+        $("#BotonesModalNominaEmpleado").empty().append('<button class="btn btn-sm btn-modal-guardar" onclick="ActualizarNominaEmpleado()">Guardar Cambios</button>');
     }
 }
+
+
+function Limpiardatos() {
+    $('#datosNominaEmpleado').hide();
+    $('#txtEmpresaNominaEmpleado').text('');
+    $('#txtSalarioNominaEmpleado').text('');
+    $('#txtSueldoaPagarNominaEmpleado').text('');
+    $('#txtAuxTransporteNominaEmpleado').text('');
+    $('#txtHEDiurnaNominaEmpleado').text('');
+    $('#txtHENocturnaNominaEmpleado').text('');
+    $('#txtHEDiurnaDFNominaEmpleado').text('');
+    $('#txtHENocturnaDFNominaEmpleado').text('');
+    $('#txtOtrosIngresosNominaEmpleado').text('');
+    $('#txtDesembolsoPrestamoNominaEmpleado').text('');
+    $('#txtTotalIngresosNominaEmpleado').text('');
+
+    $('#txtEpsNominaEmpleado').text('');
+    $('#txtPensionNominaEmpleado').text('');
+    $('#txtCasinoNominaEmpleado').text('');
+    $('#txtCobroPrestamoNominaEmpleado').text('');
+    $('#txtOtrosDescuentosNominaEmpleado').text('');
+    $('#txtTotalDescuentosNominaEmpleado').text('');
+
+    $('#txtTotalPagoNominaEmpleado').text('');
+}     
 
 
 
@@ -153,4 +178,162 @@ function CrearNominaEmpleado() {
             },
         });
     }
+}
+
+
+function GridNominaEmpleado() {
+    var tituloReporte = 'LISTADO NOMINA EMPLEADOS';
+    let datatable = $('#gridNominaEmpleado').DataTable({
+        responsive: false,
+        scrollCollapse: true,
+        scrollY: '800px',
+        scrollX: true,
+        dom: 'B<"clear">frtip',
+        columnDefs: [
+            { targets: [0], width: '10px', className: 'dt-center dt-head-center' },
+            { targets: [1], className: 'dt-head-center' },
+            { targets: [2], className: 'dt-head-center' },
+            { targets: [3], className: 'dt-head-center' },
+            { targets: [4], className: 'dt-head-center' },
+            { targets: [5], width: '10px', className: 'dt-center dt-head-center' },
+            { targets: [6], width: '10px', className: 'dt-center dt-head-center' }
+        ],
+        buttons: [
+
+            {
+                extend: 'excel', className: 'btn-excel-datatable',
+                footer: true,
+                title: tituloReporte + ' ' + NameApp,
+                filename: NameApp + ' - ' + tituloReporte + ' ' + jsDate + ' ' + hora,
+                text: '<i class="bi-file-earmark-excel-fill" style="color:green"></i> Descargar Excel',
+                exportOptions: {
+                    columns: [0, 1, 2, 3],
+                },
+            },
+            {
+                extend: 'pdfHtml5', className: 'btn-pdf-datatable',
+                text: '<i class="bi-file-earmark-pdf-fill" style="color:red"></i> Descargar Pdf',
+                filename: tituloReporte + ' - ' + NameApp + ' ' + jsDate + ' ' + hora,
+                orientation: 'portrait', // landscape
+                pageSize: 'letter', //A3 , A5 , A6 , legal , letter, A4
+                exportOptions: {
+                    columns: [0, 1, 2, 3],
+                    search: 'applied',
+                    order: 'applied',
+                },
+                customize: function (doc) {
+                    doc.content.splice(0, 1.5);
+                    doc.pageMargins = [40, 60, 20, 30];
+                    doc.defaultStyle.fontSize = 6;
+                    doc.styles.tableHeader.fontSize = 7;
+                    doc['header'] = (function () {
+                        return {
+                            columns: [
+                                {
+                                    italics: true,
+                                    fontSize: 10,
+                                    text: tituloReporte,
+                                    margin: [30, 18]
+                                }
+                            ],
+                            margin: 20
+                        }
+                    });
+                    doc['footer'] = (function (page, pages) {
+                        return {
+                            columns: [
+                                {
+                                    fontSize: 5,
+                                    alignment: 'left',
+                                    text: ' ' + now
+                                },
+                                {
+                                    fontSize: 5,
+                                    alignment: 'right',
+                                    text: ['page ', { text: page.toString() }, ' of ', { text: pages.toString() }]
+                                }
+                            ],
+                            margin: 20
+                        }
+                    });
+                    var objLayout = {};
+                    objLayout['hLineWidth'] = function (i) { return .5; };
+                    objLayout['vLineWidth'] = function (i) { return .5; };
+                    objLayout['hLineColor'] = function (i) { return '#aaa'; };
+                    objLayout['vLineColor'] = function (i) { return '#aaa'; };
+                    objLayout['paddingLeft'] = function (i) { return 4; };
+                    objLayout['paddingRight'] = function (i) { return 4; };
+                    doc.content[0].layout = objLayout;
+                }
+            },
+            {
+                text: 'Nuevo',
+                className: 'btn-nuevo-datatable',
+                action: function (e, dt, node, config) {
+                    ModalNominaEmpleado('C');
+                }
+            }
+
+        ],
+        /*"order": [[1, "asc"]],*/
+        destroy: true,
+        "ajax": {
+            "url": '/Nomina_Empleado/GridNominaEmpleado',
+            "type": "GET",
+            "datatype": "json"
+        },
+        columns: [
+            {
+                title: "Estado",
+                data: "Estado",
+                "render": function (data, type, row) {
+
+                    if (row.IdEstado == 1) {
+                        return '<label class="label-estado-activo">' + data + '</label>';
+                    }
+                    else if (row.IdEstado == 2) {
+                        return '<label class="label-estado-inactivo">' + data + '</label>';
+                    }
+                }
+
+            },
+            { "data": "Empleado", title: "Empleado", width: 'auto' },
+            { "data": "Sueldo", title: "Sueldo", width: 'auto' },
+            { "data": "CreateBy", title: "Creado Por", width: 'auto', visible: true },
+            { "data": "DateCreate", title: "Fecha Creación", width: 'auto', visible: true },
+            {
+                title: "",
+                data: null,
+                defaultContent:
+                    '<a class="EditarNominaEmpleado btn btn-editar-dt" title="editar registro"><i class="bi-pencil-fill"></i></a>',
+                orderable: false,
+            },
+            {
+                title: "",
+                data: null,
+                defaultContent:
+                    '<a class="EliminarNominaEmpleado btn btn-eliminar-dt" title="Eliminar Registro"><i class="bi-trash-fill"></i></a>',
+                orderable: false,
+            },
+        ],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.11.2/i18n/es_es.json"
+        },
+        lengthMenu: [
+            [10, 25, 50, -1],
+            ['10 Filas', '25 Filas', '50 Filas', 'Ver Todo']
+        ],
+    });
+
+    $('#gridNominaEmpleado').on('click', '.EditarNominaEmpleado', function () {
+        let data = datatable.row($(this).parents()).data();
+        ModalNominaEmpleado('E');
+        $('#LabelIdNominaEmpleado').text(data.Id);
+       
+    })
+
+    $('#gridNominaEmpleado').on('click', '.EliminarNominaEmpleado', function () {
+        let data = datatable.row($(this).parents()).data();
+        EliminarNominaEmpleado(data.Id);
+    })
 }
