6828be265f8781c7f18ac4071079278978cd1a68
oscar.castillo <oscar.castillo@DESKTOP-98NLHSV>
2025-08-19 15:29:47 -0500

    xd

---
+++ App/JsApp/Cotizacion.js
@@ -0,0 +1,18 @@
+﻿
+function ModalCotizacion(tipo) {
+    $("#TituloModalBanco").empty().val('');
+    $("#LabelIdBanco").empty().val('');
+    $("#InputNombreBanco").empty().val('');
+    $("#BotonesModalBanco").empty();
+    if (tipo == 'C') {
+        $("#TituloModalBanco").empty().append('<label>CREAR BANCO</label>');
+        $('#ModalCotizacion').modal('show');
+        $("#SelectEstadoBanco").hide();
+        $("#BotonesModalBanco").empty().append('<button type="button" class="btn btn-sm btn-modal-Cancelar" data-bs-dismiss="modal" aria-label="Close">Cancelar</button>' + '<button type="button" class="btn btn-sm btn-modal-guardar" onclick="CrearBanco()">Guardar</button>');
+    } if (tipo == 'E') {
+        $("#TituloModalBanco").empty().append('<label>EDITAR BANCO</label>');
+        $('#ModalCotizacion').modal('show');
+        $("#SelectEstadoBanco").show();
+        $("#BotonesModalBanco").empty().append('<button type="button" class="btn btn-sm btn-modal-Cancelar" data-bs-dismiss="modal" aria-label="Close">Cancelar</button>' + '<button type="button" class="btn btn-sm btn-modal-guardar" onclick="ActualizarBanco()">Guardar Cambios</button>');
+    }
+}
